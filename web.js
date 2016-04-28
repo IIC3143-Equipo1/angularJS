9731a12220cfdef529ec8647e5aa836d47445faa
Juan Diego <jddm11@gmail.com>
2016-04-27 21:45:16 -0300

    Postgres test

--- web.js
+++ web.js
@@ -18,6 +18,19 @@
 	var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
 	query.on('end', function() { client.end(); });*/	
 
+app.get('/test', function(req, res) {
+// get the total number of visits today (including the current visit)
+      client.query('SELECT * FROM items ORDER BY text', function(err, result) {
+
+        // handle an error from the query
+        if(handleError(err)) return;
+
+        // return the client to the connection pool for other requests to reuse
+        done();
+        res.json({ message: result.rows[0] });
+      });
+});
+
 app.get('/connect', function(req, res) {
 	/*pg.connect(process.env.DATABASE_URL, function(err, client) {
 	  if (err) throw err;
@@ -33,9 +46,20 @@ app.get('/connect', function(req, res) {
 
 	var client = new pg.Client(connectionString);
 	client.connect();
-	var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
-	query.on('end', function() { 
-		console.log('Created')
+	//var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+	client.query("INSERT INTO items(text,complete) values($1, $2)", ['Test Number One', true]);
+    client.query("INSERT INTO items(text,complete) values($1, $2)", ['Test Number Two', false]);
+
+    var query = client.query("SELECT * FROM items ORDER BY text");
+	query.on("row", function (row, result) {
+	    result.addRow(row);
+	});
+
+	query.on('end', function(result) { 
+		//console.log('Created')
+		 console.log(JSON.stringify(result.rows, null, "    "));
+		 JSON.stringify(result.rows, null, "    ");
+		 res.json({ message: 'Object created!' });
 		client.end(); 
 	});
 });
