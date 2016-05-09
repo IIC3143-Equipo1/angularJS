f27934cdb7fea6ce35cf19ae6cf95e7be57b0af6
PabloMessina <pamessina@uc.cl>
2016-05-09 00:52:42 -0300

    remove non-spa stuff

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,5 @@
-var models = require("../models");
-
 exports.index = function(req, res) {
     res.render('index', {
         title : 'Express'
     });
 };
-
-
-
-
-
--- web.js
+++ web.js
@@ -3,23 +3,22 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-var models = require("./server/models");
 var routes = require("./server/routes");
-var survey = require("./server/routes/survey");
 var cookieParser = require('cookie-parser');
 var session = require('cookie-session');
 var csrf = require('csurf');
 
 var app = express();
-var api_prefix = '/api/v1.0'
 
-app.set('port', process.env.PORT || 5000);
-//app.use(express.static("" + __dirname + "/dist"));
+app.set('port', process.env.PORT || 5001);
 app.use(express.static(path.join(__dirname, 'app')));
 
 // development only
 if ('development' == app.get('env')) {
-	app.use(bodyParser());
+	app.use(bodyParser.urlencoded({
+		extended: true
+	}))
+	app.use(bodyParser.json());
 	app.use(methodOverride());
 	app.use(function(err, req, res, next) {
 	  console.log(err);
@@ -39,21 +38,6 @@ app.use(function (req, res, next) {
 
 app.get('/', routes.index);
 
-app.get(api_prefix + '/survey',        survey.allSurveis);
-app.post(api_prefix + '/survey', 	   survey.createSurvey);
-app.get(api_prefix + '/survey/:id',    survey.getSurvey);
-app.put(api_prefix + '/survey/:id',    survey.updateSurvey);
-app.delete(api_prefix + '/survey/:id', survey.deleteSurvey);
-
-// middleware to use for all requests
-app.use(function(req, res, next) {
-    // do logging
-    console.log('Something is happening.');
-    next(); // make sure we go to the next routes and don't stop here
-});
-
-models.sequelize.sync().then(function () {
-  var server = app.listen(app.get('port'), function() {
-    debug('Express server listening on port ' + server.address().port);
-  });
+var server = app.listen(app.get('port'), function() {
+	console.log('Express server listening on port ' + server.address().port);
 });
